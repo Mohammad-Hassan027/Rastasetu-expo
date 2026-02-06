11802a95e7770fca51bd8414ee895c645169639d
Mohammad-Hassan027 <shaikhmohammadhassan027@gmail.com>
2026-02-06 17:31:57 +0530

    feat: add check for web

--- frontend/config/firebase.ts
+++ frontend/config/firebase.ts
@@ -1,6 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import {
+  initializeAuth,
+  getAuth,
+  // @ts-ignore - Hidden in RN type definitions
+  browserLocalPersistence,
+  // @ts-ignore - Hidden in Web type definitions
+  getReactNativePersistence,
+} from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD2OUk5HqDlkeXBLUiocbDTf4OGrxrteTM",
@@ -12,20 +20,16 @@ const firebaseConfig = {
   measurementId: "G-G6RRPQ5LKM",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth with AsyncStorage persistence for React Native
+// Select persistence based on platform
+const persistence =
+  Platform.OS === "web"
+    ? browserLocalPersistence
+    : getReactNativePersistence(AsyncStorage);
+
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
+  persistence: persistence,
 });
 
 export default app;
-
-// apiKey: "AIzaSyD2OUk5HqDlkeXBLUiocbDTf4OGrxrteTM",
-//   authDomain: "rastasetu.firebaseapp.com",
-//   projectId: "rastasetu",
-//   storageBucket: "rastasetu.firebasestorage.app",
-//   messagingSenderId: "418007313547",
-//   appId: "1:418007313547:web:a76abc58cc7d09f030788e",
-//   measurementId: "G-G6RRPQ5LKM"
